b897752bfb67aa080c8c5e601c35d9ad33d8c738
jungwonalicia <aliciawill@naver.com>
2019-05-19 13:33:59 +0900

    Add files via upload

---
+++ java02/src/제어문/입력테스트10.java
@@ -0,0 +1,24 @@
+package 제어문;
+
+import java.util.Scanner;
+
+public class 입력테스트10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("------------------------");
+		System.out.print("숫자1 입력: ");
+		int num1 = sc.nextInt();
+		System.out.print("숫자2 입력: ");
+		int num2 = sc.nextInt();
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+	}
+}
+
+
+
+
+
